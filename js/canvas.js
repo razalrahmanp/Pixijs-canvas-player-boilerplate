b43fc09b69f4d6ac4681b3d1ab3e2b0aa78ee8d8
razalrahmanp <razalrahmanp@gmail.com>
2022-07-19 18:30:34 +0530

    update-1

--- js/canvas.js
+++ js/canvas.js
@@ -2,24 +2,44 @@ let app;
 
 window.onload = function() {
 
-    app = new PIXI.Application({
+    const app = new PIXI.Application({
         width: innerWidth,
         height: innerHeight,
-        backgroundColor: 0xAAAAAA
+        backgroundColor: 0x1099bb,
+        resolution: window.devicePixelRatio || 1,
     });
     document.body.appendChild(app.view);
-    // player Objects
-    player = new PIXI.Sprite.from("images/player.png");
-    player.anchor.set(0.5);
-    player.x = app.view.width / 2;
-    player.y = app.view.height / 2;
 
-    app.stage.addChild(player);
+    const container = new PIXI.Container();
 
-    // mouse Interactions
-    app.stage.tnteractive = true;
-    app.stage.on("pointermove", movePlayer);
+    app.stage.addChild(container);
+
+    // Create a new texture
+    const texture = PIXI.Texture.from('images/player.png');
+
+    // Create a 5x5 grid of bunnies
+    for (let i = 0; i < 25; i++) {
+        const bunny = new PIXI.Sprite(texture);
+        bunny.anchor.set(0.5);
+        bunny.x = (i % 5) * 40;
+        bunny.y = Math.floor(i / 5) * 40;
+        container.addChild(bunny);
+    }
+
+    // Move container to the center
+    container.x = app.screen.width / 2;
+    container.y = app.screen.height / 2;
+
+    // Center bunny sprite in local container coordinates
+    container.pivot.x = container.width / 2;
+    container.pivot.y = container.height / 2;
+
+    // Listen for animate update
+    app.ticker.add((delta) => {
+        // rotate the container!
+        // use delta to create frame-independent transform
+        container.rotation -= 0.05 * delta;
+    });
+
 
 }
-
-// function movePlayer(e)
